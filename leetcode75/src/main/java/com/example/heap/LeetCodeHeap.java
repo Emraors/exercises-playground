63450809523cf6042a0a67b3ddc9d55d41629d1e
Emraors <eros.martinelli@hotmail.it>
2025-12-12 00:07:11 +0100

    Finish heap problems.

--- leetcode75/src/main/java/com/example/heap/LeetCodeHeap.java
+++ leetcode75/src/main/java/com/example/heap/LeetCodeHeap.java
@@ -1,23 +1,83 @@
 package com.example.heap;
 
+import java.util.Arrays;
 import java.util.PriorityQueue;
 
 public class LeetCodeHeap {
 
-	public int findKthLargest(int[] nums, int k) {
-		PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
 
-		for (int num : nums) {
-			priorityQueue.offer(num);
-			if (priorityQueue.size() > k) {
-				priorityQueue.poll();
-			}
-		}
-		return priorityQueue.poll();
-	}
+        for (int num : nums) {
+            priorityQueue.offer(num);
+            if (priorityQueue.size() > k) {
+                priorityQueue.poll();
+            }
+        }
+        Integer res = priorityQueue.peek();
+        return res != null ? res : Integer.MIN_VALUE;
+    }
 
-	public long maxScore(int[] nums1, int[] nums2, int k) {
-		return 0L;
-	}
+    public long maxScore(int[] nums1, int[] nums2, int k) {
+        int n = nums1.length;
+        Integer[] indexes = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            indexes[i] = i;
+        }
+        Arrays.sort(indexes, (a, b) -> Integer.compare(nums2[b], nums2[a]));
 
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        long sum = 0L;
+        long best = 0L;
+
+        for (int id : indexes) {
+            int x1 = nums1[id];
+            int x2 = nums2[id];
+            minHeap.offer(x1);
+            sum += x1;
+            if (minHeap.size() > k) {
+                Integer removed = minHeap.poll();
+                if (removed != null) sum -= removed;
+            }
+            if (minHeap.size() == k) {
+                best = Math.max(best, sum * (long) x2);
+            }
+        }
+        return best;
+    }
+
+    public long totalCost(int[] costs, int k, int candidates) {
+        int nWorkers = costs.length;
+        long totalCost = 0;
+        var leftHeap = new PriorityQueue<Integer>();
+        var rightHeap = new PriorityQueue<Integer>();
+
+        int i = 0, j = nWorkers - 1;
+        for (int c = 0; c < candidates && i <= j; c++) {
+            leftHeap.offer(costs[i++]);
+        }
+        for (int c = 0; c < candidates && i <= j; c++) {
+            rightHeap.offer(costs[j--]);
+        }
+
+        for (int round = 0; round < k; round++) {
+            var leftMin = leftHeap.peek();
+            var rightMin = rightHeap.peek();
+
+            if (leftMin == null && rightMin == null) break;
+            if (rightMin == null || (leftMin != null && leftMin <= rightMin)) {
+                totalCost += leftHeap.poll();
+                if (i <= j) {
+                    leftHeap.offer(costs[i++]);
+                }
+            }
+            else {
+                totalCost += rightHeap.poll();
+                if (i <= j) {
+                    rightHeap.offer(costs[j--]);
+                }
+            }
+        }
+        return totalCost;
+    }
 }
--- leetcode75/src/test/java/com/example/heap/LeetCodeHeapTest.java
+++ leetcode75/src/test/java/com/example/heap/LeetCodeHeapTest.java
@@ -9,7 +9,7 @@ class LeetCodeHeapTest {
 	private final LeetCodeHeap leetCode = new LeetCodeHeap();
 
 	@Test
-	void findKthLargest() {
+	void testFindKthLargest() {
 		int[] firstTest = new int[] { 3, 2, 1, 1, 1, 1, 5, 6, 4 };
 		int[] secondTest = new int[] { 3, 2, 3, 1, 2, 4, 5, 5, 6 };
 
@@ -18,7 +18,7 @@ class LeetCodeHeapTest {
 	}
 
 	@Test
-	void maxScore() {
+	void testMaxScore() {
 		int[] firstTest1 = new int[] { 1, 3, 3, 2 };
 		int[] firstTest2 = new int[] { 2, 1, 3, 4 };
 
@@ -27,5 +27,16 @@ class LeetCodeHeapTest {
 
 		assertEquals(12L, leetCode.maxScore(firstTest1, firstTest2, 3));
 		assertEquals(30L, leetCode.maxScore(secondTest1, secondTest2, 1));
+	}
+
+	@Test
+	void testTotalCost() {
+		int[] firstTest = new int[]{17, 12, 10, 2, 7, 2, 11, 20, 8};
+		int[] secondTest = new int[]{1, 2, 4, 1};
+		int[] thirdTest = new int[]{31,25,72,79,74,65,84,91,18,59,27,9,81,33,17,58};
+
+		//assertEquals(11, leetCode.totalCost(firstTest, 3, 4));
+		assertEquals(4, leetCode.totalCost(secondTest, 3, 3));
+		assertEquals(423, leetCode.totalCost(thirdTest, 11, 2));
 	}
 }
