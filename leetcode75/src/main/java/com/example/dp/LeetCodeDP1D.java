e346af7c0d79f47eb4f7f3ee367186cf86bc450a
Emraors <eros.martinelli@hotmail.it>
2025-12-17 20:57:43 +0100

    Finish rob

--- leetcode75/src/main/java/com/example/dp/LeetCodeDP1D.java
+++ leetcode75/src/main/java/com/example/dp/LeetCodeDP1D.java
@@ -1,11 +1,19 @@
 package com.example.dp;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class LeetCodeDP1D {
 
 	private final Map<Integer, Integer> memoizationTrib = new HashMap<>();
+	private final Map<Set<Integer>, Integer> firstMemoizationRob = new HashMap<>();
+
+	public void cleanState() {
+		memoizationTrib.clear();
+		firstMemoizationRob.clear();
+	}
 
 	public int tribonacci(int n) {
 		if (n == 0)
@@ -20,5 +28,82 @@ public class LeetCodeDP1D {
 		int result = tribonacci(n - 3) + tribonacci(n - 2) + tribonacci(n - 1);
 		memoizationTrib.put(n, result);
 		return result;
+	}
+
+	public int minCostClimbingStairs(int[] cost) {
+		int length = cost.length;
+
+		if (length == 0)
+			return 0;
+		if (length == 1)
+			return cost[0];
+
+		int[] costPath = new int[length];
+
+		costPath[length - 1] = cost[length - 1];
+		costPath[length - 2] = cost[length - 2];
+
+		for (int i = length - 3; i >= 0; i--) {
+			costPath[i] = cost[i] + Math.min(costPath[i + 1], costPath[i + 2]);
+		}
+
+		return Math.min(costPath[0], costPath[1]);
+	}
+
+	public int rob(int[] nums) {
+		int length = nums.length;
+
+		if (length == 0)
+			return 0;
+		if (length == 1)
+			return nums[0];
+
+		int[] mem = new int[length];
+		mem[0] = nums[0];
+		mem[1] = Math.max(nums[1], nums[0]);
+
+		for (int i = 2; i < length; i++) {
+			mem[i] = Math.max(nums[i] + mem[i - 2], mem[i - 1]);
+		}
+		return mem[length - 1];
+	}
+
+	//This was my first solution, but it is inefficient
+	public int robInefficient(int[] nums) {
+		if (nums.length == 0)
+			return 0;
+		if (nums.length == 1)
+			return nums[0];
+
+		Set<Integer> allowedIndexes = new HashSet<>();
+		for (int i = 0; i < nums.length; i++) {
+			allowedIndexes.add(i);
+			firstMemoizationRob.put(Set.of(i), nums[i]);
+		}
+		return robRecSupp(allowedIndexes, nums);
+	}
+
+	private int robRecSupp(Set<Integer> allowedIndexes, int[] nums) {
+		if (allowedIndexes.isEmpty()) {
+			return 0;
+		}
+
+		Integer cached = firstMemoizationRob.get(allowedIndexes);
+		if (cached != null)
+			return cached;
+
+		int max = 0;
+		for (Integer index : allowedIndexes) {
+			Set<Integer> newAllowed = new HashSet<>(allowedIndexes);
+			newAllowed.remove(index);
+			newAllowed.remove(index - 1);
+			newAllowed.remove(index + 1);
+
+			int value = nums[index] + robRecSupp(newAllowed, nums);
+			max = Math.max(max, value);
+		}
+
+		firstMemoizationRob.put(allowedIndexes, max);
+		return max;
 	}
 }
--- leetcode75/src/test/java/com/example/dp/LeetCodeDP1DTest.java
+++ leetcode75/src/test/java/com/example/dp/LeetCodeDP1DTest.java
@@ -14,4 +14,35 @@ class LeetCodeDP1DTest {
 		assertEquals(4, leetCode.tribonacci(4));
 		assertEquals(1389537, leetCode.tribonacci(25));
 	}
+
+	@Test
+	void testMinCostClimbingStairs() {
+
+		int[] firstTest = new int[] { 10, 15, 20 };
+		int[] secondTest = new int[] { 1, 100, 1, 1, 1, 100, 1, 1, 100, 1 };
+
+		assertEquals(15, leetCode.minCostClimbingStairs(firstTest));
+		assertEquals(6, leetCode.minCostClimbingStairs(secondTest));
+	}
+
+	@Test
+	void testRob() {
+
+		int[] firstTest = new int[] { 1, 2, 3, 1 };
+		int[] secondTest = new int[] { 2, 7, 9, 3, 1 };
+		int[] thirdTest = new int[] { 2, 1, 1, 2 };
+		int[] fourthTest = new int[] { 100, 10, 1, 10, 100 };
+		int[] fifthTest = new int[] { 183, 219, 57, 193, 94, 233, 202, 154, 65, 240, 97, 234, 100, 249, 186, 66, 90,
+				238, 168, 128, 177, 235, 50, 81, 185, 165, 217, 207, 88, 80, 112, 78, 135, 62, 228, 247, 211 };
+
+		assertEquals(4, leetCode.rob(firstTest));
+		leetCode.cleanState();
+		assertEquals(12, leetCode.rob(secondTest));
+		leetCode.cleanState();
+		assertEquals(4, leetCode.rob(thirdTest));
+		leetCode.cleanState();
+		assertEquals(201, leetCode.rob(fourthTest));
+		leetCode.cleanState();
+		assertEquals(3365, leetCode.rob(fifthTest));
+	}
 }
